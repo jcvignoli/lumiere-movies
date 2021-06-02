48a0c8670167c734a51806e9101bc2270095e25f
jcvignoli <jcv@jcvignoli.com>
2021-06-02 12:23:20 -0400

    fixed require_once with full path (__DIR__)

--- inc/options-general.php
+++ inc/options-general.php
@@ -27,7 +27,7 @@ $messages = array( /* highslide message notification options */
 );
 
 // included files
-require_once ( $imdb_admin_values['imdbplugindirectory'] . 'class/functions.php');
+require_once (plugin_dir_path( __DIR__ ) . 'class/functions.php');
 
 // If $_GET["msg"] is found, display a related notice
 if ((isset($_GET['msg'])) && array_key_exists( sanitize_key( $_GET['msg'] ), $messages ) ){
