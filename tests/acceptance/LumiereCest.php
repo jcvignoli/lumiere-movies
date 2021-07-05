b8ca5731693a17f10f55aab2ad03fafc8edf3f73
jcvignoli <dev@jcvignoli.com>
2021-07-05 19:43:18 -0400

    finalised codeception test environment

--- tests/acceptance/LumiereCest.php
+++ tests/acceptance/LumiereCest.php
@@ -1,25 +1,56 @@
 <?php
 class LumiereCest {
-/*
+
 	public function frontpageWorks(AcceptanceTester $I) {
+		$I->wantTo('check frontpage');
 		$I->amOnPage('/');
 		$I->see('Here you are');	
 	}
-*/
-	public function popupmoviesWorks(AcceptanceTester $I) {
-		$I->wantTo('Test if popup movies works');
-		$I->amOnPage('/2021/test-imdblt/');
-			$element = 'a[data-highslidefilm="fight+club"]';
-		$I->executeJS( "return jQuery('" . $element . "').get(0).click()");
-		$I->see('David Fincher');
-	}
-/* check
-	public function popupmoviesWorks(AcceptanceTester $I) {
-		$I->wantTo('Test if popup movies works');
+
+
+	public function popupMoviesCanOpen(AcceptanceTester $I, \Codeception\Scenario $scenario) {
+		/* vars */
+		// Get url depending on the environment called in codeception.yml
+		$current_env = $scenario->current('env');
+		$config = \Codeception\Configuration::config();
+		$url_base = $config['env'][$current_env]['modules']['enabled'][0]['WebDriver']['url'];
+		// $url = $_ENV['TEST_SITE_WP_URL']; # more direct, but can be wrong
+		// popup link movie interstellar
+		$element = 'a[data-highslidefilm="interstellar"]';
+		$sub_url = '/imdblt/film/interstellar/?film=interstellar';
+
+		$I->wantTo('popup movies can open');
 		$I->amOnPage('/2021/test-codeception/');
-			$element = 'a[data-highslidefilm="interstellar"]';
 		$I->executeJS( "return jQuery('" . $element . "').get(0).click()");
+		$I->wait(5);
+		$I->switchToIFrame("//iframe[@src='$url_base$sub_url']");
 		$I->see('Christopher Nolan');
 	}
-*/
+
+	public function popupPersonCanOpen(AcceptanceTester $I, \Codeception\Scenario $scenario) {
+		/* vars */
+		// Get url depending on the environment called in codeception.yml
+		$current_env = $scenario->current('env');
+		$config = \Codeception\Configuration::config();
+		$url_base = $config['env'][$current_env]['modules']['enabled'][0]['WebDriver']['url'];
+		// $url = $_ENV['TEST_SITE_WP_URL']; # more direct, but can be wrong
+		// popup link actor Jorge Rivero
+		$element = 'a[data-highslidepeople="0729473"]';
+		$sub_url = '/imdblt/person/0729473/?mid=0729473';
+
+		$I->wantTo('popup person can open');
+		$I->amOnPage('/2021/test-codeception/');
+		$I->executeJS( "return jQuery('" . $element . "').get(0).click()");
+		$I->wait(5);
+		$I->switchToIFrame("//iframe[@src='$url_base$sub_url']");
+		$I->see('Pajarero');
+	}
+
+	public function taxonomyPage(AcceptanceTester $I) {
+		$I->wantTo('taxonomy person page');
+		$I->amOnPage('/2021/test-codeception/');
+		$I->click( "Tony Zarindast");
+		$I->see('Tehran');
+	}
+
 }
