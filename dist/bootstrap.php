6a49151a32a7cc5f89d2b79adbf31c7bd90784b8
jcvignoli <dev@jcvignoli.com>
2021-08-18 16:26:27 -0400

    copy taxo template: added nonce verif, renamed class files to match wordpress standards

--- dist/bootstrap.php
+++ dist/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
-#	Vendor Libraries (notably IMDbPHP)
+#	Vendor Libraries with autoload (notably IMDbPHP)
 include_once __DIR__ . '/vendor/autoload.php';
 
 #	Lumiere Classes (no autoload, don't want all classes)
 include_once __DIR__ . '/class/utils.php';
 include_once __DIR__ . '/class/core.php';
-include_once __DIR__ . '/class/Config.php';
+include_once __DIR__ . '/class/settings.php';
 include_once __DIR__ . '/class/movie.php';
 include_once __DIR__ . '/class/metabox.php';
 include_once __DIR__ . '/class/widget.php';
