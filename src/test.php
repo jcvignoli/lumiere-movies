fd49870857dbb9e7ef08abcb7055eaf36c3ec070
jcvignoli <dev@jcvignoli.com>
2021-08-23 01:31:54 -0400

    test precommit

---
+++ src/test.php
@@ -0,0 +1,17 @@
+<?php declare( strict_types = 1 );
+/**
+ * Bootstrap of selected files
+ * @phpcs:disable PEAR.Files.IncludingFile
+ */
+     
+#	Vendor Libraries with autoload (notably IMDbPHP)
+include_once __DIR__ . '/vendor/autoload.php';
+
+#	Lumiere Classes (no autoload, don't want all classes)
+include_once __DIR__ . '/class/utils.php';
+include_once __DIR__ . '/class/core.php';
+include_once __DIR__ . '/class/settings.php';
+include_once __DIR__ . '/class/movie.php';
+include_once __DIR__ . '/class/metabox.php';
+include_once __DIR__ . '/class/widget.php';
+
