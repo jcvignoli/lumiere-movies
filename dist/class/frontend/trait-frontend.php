b100f17e5220a2bdf54f7124fa1d6f49225d3ec1
jcvignoli <dev@jcvignoli.com>
2022-05-22 12:48:37 -0300

    fixed wrong factory method declaration, fixed tests names

--- dist/class/frontend/trait-frontend.php
+++ dist/class/frontend/trait-frontend.php
@@ -32,7 +32,6 @@ trait Frontend {
 	 *
 	 * @since 3.7
 	 * @var array<int, string>
-	 * @phpstan-ignore-next-line PHPStan complains that var is not defined for some contexts
 	 */
 	public array $plugins_in_use = [];
 
